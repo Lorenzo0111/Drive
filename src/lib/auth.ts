b170b359f3fa702fddefc33f103fc250dea50eec
Lorenzo <hello@lorenzo0111.me>
2024-08-13 09:32:10 +0200

    Implemented auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,30 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { DefaultSession } from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { prisma } from "./prisma";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+});
